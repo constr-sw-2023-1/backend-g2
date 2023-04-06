acb2d5b326b96f22d2b7c704bc75467e544d8438
Lucas Buffet <lebuffet02@gmail.com>
2023-04-05 23:25:26 -0300

    inicializando o projeto

---
+++ backend/src/main/java/br/com/grupo2/backend/aulas/api/Application.java
@@ -0,0 +1,13 @@
+package br.com.grupo2.backend.aulas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/grupo2/backend/aulas/api/BackendApplicationTest.java
@@ -0,0 +1,12 @@
+//package br.com.grupo2.backend;
+//
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@ExtendWith((MockitoExtension.class))
+//class BackendApplicationTest {
+//
+//
+//}
